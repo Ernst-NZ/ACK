b62da7c1586573549198863ec4b48268d59d12f0
Ernst-NZ <ernst@hotmail.co.nz>
2018-12-22 19:22:53 +1300

    22-12 19:22

--- src/app/lidmate/lidmate-lys/filter-textbox.component.ts
+++ src/app/lidmate/lidmate-lys/filter-textbox.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-filter-textbox',
-    template: `    Soek: <input type="text" />
+    template: `    Soek: <input style="width: 60%;" type="text" />
 `
 })
 export class FilterTextboxComponent implements OnInit {
--- src/app/lidmate/lidmate-lys/lidmate-lys.component.ts
+++ src/app/lidmate/lidmate-lys/lidmate-lys.component.ts
@@ -16,10 +16,10 @@ export class LidmateLysComponent implements OnInit {
     
     ngOnInit() {
         this.filteredLidmate = [
-            {LidmaatId:1, FirstName: 'John', LastName: 'Doe', NickName: 'John', Gemeente: 'Tauranga' },
-            {LidmaatId:2, FirstName: 'Koos', LastName: 'Nel', NickName: 'Koos', Gemeente: 'Tauranga' },
-            {LidmaatId:3, FirstName: 'Piet', LastName: 'Venter', NickName: 'Piet', Gemeente: 'Tauranga' },
-            {LidmaatId:4, FirstName: 'Gert', LastName: 'DBothaoe', NickName: 'Gert', Gemeente: 'Tauranga' }
+            {LidmaatId:1, FirstName: 'John', LastName: 'Doe', NickName: 'John', Gemeente: 'Tauranga', LastVisit: '01 Jan 2018' },
+            {LidmaatId:2, FirstName: 'Koos', LastName: 'Nel', NickName: 'Koos', Gemeente: 'Tauranga' , LastVisit: '02 Feb 2018' },
+            {LidmaatId:3, FirstName: 'Piet', LastName: 'Venter', NickName: 'Piet', Gemeente: 'Tauranga' , LastVisit: '03 Mar 2018' },
+            {LidmaatId:4, FirstName: 'Gert', LastName: 'Botha', NickName: 'Gert', Gemeente: 'Tauranga' , LastVisit: '04 Apr 2018' }
           ];
 
     }
