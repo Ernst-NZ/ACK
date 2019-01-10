5efeb0b4b19b7a3c02657696f2a9819f9a1dedfd
Ernst-NZ <ernst@hotmail.co.nz>
2019-01-10 22:19:14 +1300

    csad

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,8 +5,10 @@ export class Globals {
   selectedRoute: string;
   lidmaatId: Number;
   addressId: Number;
+  lidmaatDetails: String;
 //  dataSource: "https://data.ezy.kiwi"
   dataSource: "http://localhost:3000"
+
   
 
 }
