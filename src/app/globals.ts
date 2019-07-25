c6f71d39ee17d98218ee9c8a9494a0e3b508c045
Ernst-NZ <ernst@hotmail.co.nz>
2019-07-25 21:55:46 +1200

    fix globals

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,15 +5,15 @@ export class Globals {
   selectedRoute: string;
   lidmaatId: Number;
   addressId: Number;
-  lidmaatDetails: String = "Voeg nuwe lidmaat by";
+  lidmaatDetails: String = 'Voeg nuwe lidmaat by';
   addressDetails: String;
- // dataSource: String =  "https://data.acktauranga.nz"
-  dataSource: String = "http://localhost:3000";
+  dataSource: String =  'https://data.acktauranga.nz'
+ // dataSource: String = "http://localhost:3000";
   userName: String;
-  authenticate: boolean = false;
-  adminUser: boolean = false;
-  isSyncing: boolean = false;
+  authenticate = false;
+  adminUser = false;
+  isSyncing = false;
   myFilter: String;
   filter: boolean;
-  activeOnly: string = "True";
+  activeOnly = 'True';
 }
